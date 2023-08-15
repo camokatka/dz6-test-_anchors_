2bc7a6847bb77565361e81cf20180ed2c50a9231
camokatka <elizaveta.serykh@gmail.com>
2023-08-15 14:18:40 +0300

    Initial Commit

---
+++ dz6(test)_anchors/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dz6(test)_anchors
+//
+//  Created by Elizabeth Serykh on 15.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
